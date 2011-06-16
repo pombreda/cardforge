695b8da04f603e7509cb74d7820d44b55b456168
dennis.r.friedrichsen <dennis.r.friedrichsen@d79705b8-2151-0410-8572-c3d52707f174>
2011-06-16 14:03:38 +0000

    add a new option for Dev Mode "Tutor for Card".  It allows you to search your library for a card and put it into your hand.

--- src/src/forge/GuiDisplay4.java
+++ src/src/forge/GuiDisplay4.java
@@ -179,8 +179,18 @@ public class GuiDisplay4 extends JFrame implements CardContainer, Display, NewCo
 				}
 			};
 			// - Battlefield setup -
+			
+			//DevMode Tutor
+			ForgeAction tutor = new ForgeAction(TUTOR) {
+				private static final long serialVersionUID = 2003222642609217705L;
 
-            Object[] objDev = { GuiDisplay4.canLoseByDecking, viewAIHand, viewAILibrary, viewHumanLibrary, generateMana, setupBattleField };
+				public void actionPerformed(ActionEvent arg0) {
+					GuiDisplayUtil.devModeTutor();
+				}
+			};
+			//end DevMode Tutor
+
+            Object[] objDev = { GuiDisplay4.canLoseByDecking, viewAIHand, viewAILibrary, viewHumanLibrary, generateMana, setupBattleField, tutor };
 	        for(Object o:objDev) {
 	        	if(o instanceof ForgeAction) 
 	        		devMenu.add(((ForgeAction) o).setupButton(new JMenuItem()));
