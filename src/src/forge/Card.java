0dafacdb1328afd79c4bf4a03f0830c3499bc2fd
Chris.MTG.H <Chris.MTG.H@d79705b8-2151-0410-8572-c3d52707f174>
2011-06-17 01:57:09 +0000

    - Fixed the Devour keyword and it will now appear in the card detail panel.

--- src/src/forge/Card.java
+++ src/src/forge/Card.java
@@ -996,7 +996,8 @@ public class Card extends MyObservable {
                 } else if (keyword.get(i).startsWith("Soulshift") || keyword.get(i).startsWith("Cumulative upkeep") 
                         || keyword.get(i).startsWith("Echo")      || keyword.get(i).startsWith("Fading") 
                         || keyword.get(i).startsWith("Ripple")    || keyword.get(i).startsWith("Unearth") 
-                        || keyword.get(i).startsWith("Vanishing") || keyword.get(i).startsWith("Madness")) {
+                        || keyword.get(i).startsWith("Vanishing") || keyword.get(i).startsWith("Madness")
+                        || keyword.get(i).startsWith("Devour")) {
                     String k = keyword.get(i);
                     k = k.replace(":", " ");
                     sbLong.append(k).append("\r\n");
