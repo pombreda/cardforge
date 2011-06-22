5840ffb8b7612675b13d1f253386e6cacf239b61
BraidsCC@gmail.com <BraidsCC@gmail.com@d79705b8-2151-0410-8572-c3d52707f174>
2011-06-22 21:11:43 +0000

    creating a private branch of trunk (aka src) for longer term minimax AI development.

---
+++ src/forge/AllZone.java
@@ -0,0 +1,132 @@
+package forge;
+
+
+import forge.card.cardFactory.CardFactory;
+import forge.card.mana.ManaPool;
+import forge.card.trigger.TriggerHandler;
+import forge.gui.input.InputControl;
+import forge.properties.ForgeProps;
+import forge.properties.NewConstants;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+
+public class AllZone implements NewConstants {
+    //only for testing, should read decks from local directory
+//  public static final IO IO = new IO("all-decks");
+	
+	public static final Player					HumanPlayer				= new HumanPlayer("Human");
+    public static final Player					ComputerPlayer			= new AIPlayer("Computer");
+    
+    public static forge.quest.data.QuestData     QuestData          = null;
+    public static Quest_Assignment 			     QuestAssignment    = null;
+    public static final NameChanger              NameChanger        = new NameChanger();
+    
+    public static EndOfTurn                      EndOfTurn          = new EndOfTurn();
+    public static EndOfCombat					 EndOfCombat		= new EndOfCombat();
+
+    public static final Phase                    Phase              = new Phase();
+    
+    // Phase is now a prerequisite for CardFactory
+    public static final CardFactory              CardFactory        = new CardFactory(ForgeProps.getFile(CARDSFOLDER));
+    
+    public static final MagicStack               Stack              = new MagicStack();
+    public static final InputControl             InputControl       = new InputControl();
+    public static final GameAction               GameAction         = new GameAction();
+    public static final StaticEffects        	 StaticEffects      = new StaticEffects();
+    public static final GameInfo				 GameInfo 			= new GameInfo();
+    
+    public static final TriggerHandler			 TriggerHandler		= new TriggerHandler();
+
+    //initialized at Runtime since it has to be the last object constructed
+
+    public static ComputerAI_Input Computer;
+
+    //shared between Input_Attack, Input_Block, Input_CombatDamage , InputState_Computer
+    
+    public static Combat Combat   = new Combat();
+
+    //Human_Play, Computer_Play is different because Card.comesIntoPlay() is called when a card is added by PlayerZone.add(Card)
+    public final static PlayerZone Human_Battlefield	= new PlayerZone_ComesIntoPlay(Constant.Zone.Battlefield, AllZone.HumanPlayer);
+    public final static PlayerZone Human_Hand      		= new DefaultPlayerZone(Constant.Zone.Hand      , AllZone.HumanPlayer);
+    public final static PlayerZone Human_Graveyard 		= new DefaultPlayerZone(Constant.Zone.Graveyard , AllZone.HumanPlayer);
+    public final static PlayerZone Human_Library   		= new DefaultPlayerZone(Constant.Zone.Library   , AllZone.HumanPlayer);
+    public final static PlayerZone Human_Exile   		= new DefaultPlayerZone(Constant.Zone.Exile, AllZone.HumanPlayer);
+    public final static PlayerZone Human_Command   		= new DefaultPlayerZone(Constant.Zone.Command, AllZone.HumanPlayer);
+
+    public final static PlayerZone Computer_Battlefield	= new PlayerZone_ComesIntoPlay(Constant.Zone.Battlefield      , AllZone.ComputerPlayer);
+    public final static PlayerZone Computer_Hand      	= new DefaultPlayerZone(Constant.Zone.Hand      , AllZone.ComputerPlayer);
+    public final static PlayerZone Computer_Graveyard 	= new DefaultPlayerZone(Constant.Zone.Graveyard , AllZone.ComputerPlayer);
+    public final static PlayerZone Computer_Library   	= new DefaultPlayerZone(Constant.Zone.Library   , AllZone.ComputerPlayer);
+    public final static PlayerZone Computer_Exile   	= new DefaultPlayerZone(Constant.Zone.Exile, AllZone.ComputerPlayer);
+    public final static PlayerZone Computer_Command   	= new DefaultPlayerZone(Constant.Zone.Command, AllZone.ComputerPlayer);
+    
+    public final static PlayerZone Stack_Zone   = new DefaultPlayerZone(Constant.Zone.Stack, null);
+    
+    public static final ManaPool ManaPool = new ManaPool(AllZone.HumanPlayer);
+    public static final ManaPool Computer_ManaPool = new ManaPool(AllZone.ComputerPlayer);
+    
+    public static Display Display;
+
+    private final static Map<String,PlayerZone> map = new HashMap<String,PlayerZone>();
+
+    static
+    {
+		map.put(Constant.Zone.Graveyard         + AllZone.HumanPlayer, Human_Graveyard);
+		map.put(Constant.Zone.Hand              + AllZone.HumanPlayer, Human_Hand);
+		map.put(Constant.Zone.Library           + AllZone.HumanPlayer, Human_Library);
+		map.put(Constant.Zone.Battlefield		+ AllZone.HumanPlayer, Human_Battlefield);
+		map.put(Constant.Zone.Exile 			+ AllZone.HumanPlayer, Human_Exile);
+		map.put(Constant.Zone.Command 			+ AllZone.HumanPlayer, Human_Command);
+	
+		map.put(Constant.Zone.Graveyard         + AllZone.ComputerPlayer, Computer_Graveyard);
+		map.put(Constant.Zone.Hand              + AllZone.ComputerPlayer, Computer_Hand);
+		map.put(Constant.Zone.Library           + AllZone.ComputerPlayer, Computer_Library);
+		map.put(Constant.Zone.Battlefield       + AllZone.ComputerPlayer, Computer_Battlefield);
+		map.put(Constant.Zone.Exile 			+ AllZone.ComputerPlayer, Computer_Exile);
+		map.put(Constant.Zone.Command 			+ AllZone.ComputerPlayer, Computer_Command);
+		
+		map.put(Constant.Zone.Stack				+ null					, Stack_Zone);
+		
+    }
+    
+    public static PlayerZone getZone(Card c)
+    {
+		Iterator<PlayerZone> it = map.values().iterator();
+		PlayerZone p;
+		while(it.hasNext())
+		{
+		    p = (PlayerZone)it.next();
+	
+		    if(AllZoneUtil.isCardInZone(p, c))
+		    	return p;
+		}
+		return null;
+    }
+    
+    public static PlayerZone getZone(String zone, Player player)
+    {
+    	if(zone.equals("Stack")) player = null;
+		Object o = map.get(zone + player);
+		if(o == null)
+		    throw new RuntimeException("AllZone : getZone() invalid parameters " +zone +" " +player);
+	
+		return (PlayerZone)o;
+    }
+
+    public static void resetZoneMoveTracking()
+    {
+        ((DefaultPlayerZone)Human_Command).resetCardsAddedThisTurn();
+        ((DefaultPlayerZone)Human_Library).resetCardsAddedThisTurn();
+        ((DefaultPlayerZone)Human_Hand).resetCardsAddedThisTurn();
+        ((DefaultPlayerZone)Human_Battlefield).resetCardsAddedThisTurn();
+        ((DefaultPlayerZone)Human_Graveyard).resetCardsAddedThisTurn();
+        ((DefaultPlayerZone)Computer_Command).resetCardsAddedThisTurn();
+        ((DefaultPlayerZone)Computer_Library).resetCardsAddedThisTurn();
+        ((DefaultPlayerZone)Computer_Hand).resetCardsAddedThisTurn();
+        ((DefaultPlayerZone)Computer_Battlefield).resetCardsAddedThisTurn();
+        ((DefaultPlayerZone)Computer_Graveyard).resetCardsAddedThisTurn();
+    }
+}//AllZone
---
+++ src/forge/card/spellability/Spell_Permanent.java
@@ -0,0 +1,288 @@
+
+package forge.card.spellability;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import forge.AllZone;
+import forge.AllZoneUtil;
+import forge.ButtonUtil;
+import forge.Card;
+import forge.CardList;
+import forge.Command;
+import forge.CommandReturn;
+import forge.Constant;
+import forge.Player;
+import forge.card.abilityFactory.AbilityFactory;
+import forge.card.cardFactory.CardFactory;
+import forge.card.cardFactory.CardFactoryUtil;
+import forge.card.trigger.Trigger;
+import forge.gui.input.Input;
+
+
+public class Spell_Permanent extends Spell {
+    private static final long serialVersionUID = 2413495058630644447L;
+    
+    private boolean willChampion = false;
+    private String championType = null;
+    
+    /////////////////////
+    ///////
+    private final CommandReturn championGetCreature = new CommandReturn() {
+        public Object execute() {
+            return AllZoneUtil.getPlayerTypeInPlay(getSourceCard().getController(), championType);
+        }
+    };//CommandReturn
+    
+    final SpellAbility championAbilityComes = new Ability(getSourceCard(), "0") {
+        @Override
+        public void resolve() {
+            if(getTargetCard() == null || getTargetCard() == getSourceCard()) AllZone.GameAction.sacrifice(getSourceCard());
+            
+            else if(AllZoneUtil.isCardInPlay(getTargetCard())) {
+                AllZone.GameAction.exile(getTargetCard());
+            }
+        }//resolve()
+    };
+    
+    final Input championInputComes = new Input() {
+		private static final long serialVersionUID = -7503268232821397107L;
+
+		@Override
+        public void showMessage() {
+            CardList choice = (CardList) championGetCreature.execute();
+            
+            stopSetNext(CardFactoryUtil.input_targetChampionSac(getSourceCard(), championAbilityComes, choice,
+                    "Select another "+championType+" you control to exile", false, false));
+            ButtonUtil.disableAll(); //target this card means: sacrifice this card
+        }
+    };
+    Command championCommandComes = new Command() {
+        
+        private static final long serialVersionUID = -3580408066322945328L;
+        
+        public void execute() {
+            CardList creature = (CardList) championGetCreature.execute();
+            Player s = getSourceCard().getController();
+            if(creature.size() == 0) {
+                AllZone.GameAction.sacrifice(getSourceCard());
+                return;
+            } else if(s.isHuman()) {
+            	AllZone.InputControl.setInput(championInputComes);
+            }
+            else { //Computer
+                Card target;
+                CardList computer = AllZoneUtil.getPlayerTypeInPlay(AllZone.ComputerPlayer, championType);
+                computer.remove(getSourceCard());
+                
+                computer.shuffle();
+                if(computer.size() != 0) {
+                    target = computer.get(0);
+                    championAbilityComes.setTargetCard(target);
+                    AllZone.Stack.addSimultaneousStackEntry(championAbilityComes);
+                    if(getSourceCard().getName().equals("Mistbind Clique")) {
+                    	//TODO this needs to target
+                    	CardList list = AllZoneUtil.getPlayerLandsInPlay(AllZone.HumanPlayer);
+                    	for(Card c:list) c.tap();
+                    }
+                }
+                else
+                	AllZone.GameAction.sacrifice(getSourceCard());
+            }//computer
+        }//execute()
+    };//championCommandComes
+    
+    Command championCommandLeavesPlay = new Command() {
+        
+        private static final long serialVersionUID = -5903638227914705191L;
+        
+        public void execute() {
+            //System.out.println(abilityComes.getTargetCard().getName());
+            Object o = championAbilityComes.getTargetCard();
+            
+            if(o == null || ((Card) o).isToken() || !AllZoneUtil.isCardExiled((Card) o)) return;
+            
+            SpellAbility ability = new Ability(getSourceCard(), "0") {
+                @Override
+                public void resolve() {
+                    Card c = championAbilityComes.getTargetCard();
+                    if(!c.isToken()) {
+                    	AllZone.GameAction.moveToPlay(c);
+                        
+                    }
+                }//resolve()
+            };//SpellAbility
+            
+            StringBuilder sb = new StringBuilder();
+            sb.append(getSourceCard().getName()).append(" - returning card to battlefield.");
+            ability.setStackDescription(sb.toString());
+            
+            AllZone.Stack.addSimultaneousStackEntry(ability);
+        }//execute()
+    };//championCommandLeavesPlay
+    
+    ///////
+    ////////////////////
+    
+    public Spell_Permanent(Card sourceCard) {
+    	// Add Costs for all SpellPermanents
+    	this(sourceCard, new Cost(sourceCard.getManaCost(), sourceCard.getName(), false), null);
+    }//Spell_Permanent()
+    
+    public Spell_Permanent(Card sourceCard, Cost cost, Target tgt) {
+        super(sourceCard, cost, tgt);
+        
+        if(CardFactory.hasKeyword(sourceCard,"Champion") != -1) {
+        	int n = CardFactory.hasKeyword(sourceCard, "Champion");
+            
+            String parse = sourceCard.getKeyword().get(n).toString();
+        	willChampion = true;
+        	championType = parse.split(":")[1];
+        }
+        
+        if(sourceCard.isCreature()) {
+        	
+        	StringBuilder sb = new StringBuilder();
+        	sb.append(sourceCard.getName()).append(" - Creature ").append(sourceCard.getNetAttack());
+        	sb.append(" / ").append(sourceCard.getNetDefense());
+        	setStackDescription(sb.toString());
+        }
+        else setStackDescription(sourceCard.getName());
+        
+        setDescription(getStackDescription());
+        if(willChampion) {
+        	sourceCard.addComesIntoPlayCommand(championCommandComes);
+        	sourceCard.addLeavesPlayCommand(championCommandLeavesPlay);
+        }
+        
+    }//Spell_Permanent()
+    
+    
+    @Override
+    public boolean canPlay() {
+    	Card source = getSourceCard();
+    	if(AllZone.Stack.isSplitSecondOnStack() || source.isUnCastable()) return false;
+    	
+    	Player turn = AllZone.Phase.getPlayerTurn();
+
+    	if(source.getName().equals("Serra Avenger")) {
+        	if (turn.equals(source.getController()) && turn.getTurn() <= 3)
+        		return false;
+    	}
+    	else if(source.getName().equals("Blizzard")) {
+    		CardList lands = AllZoneUtil.getPlayerLandsInPlay(source.getController());
+    		lands = lands.getType("Snow");
+    		if(lands.size() == 0) return false;
+    	}
+    
+    	// Flash handled by super.canPlay
+        return super.canPlay();
+    }
+    
+    @Override
+    public boolean canPlayAI() {
+    	
+    	Card card = getSourceCard();
+    	
+        //check on legendary
+        if (card.isType("Legendary")) {
+        	CardList list = AllZoneUtil.getPlayerCardsInPlay(AllZone.ComputerPlayer);
+            if (list.containsName(card.getName()))
+            	return false;
+        }
+        if (card.isType("Planeswalker")) {
+        	CardList list = AllZoneUtil.getPlayerCardsInPlay(AllZone.ComputerPlayer);
+        	list = list.getType("Planeswalker");
+        	
+        	for (int i=0;i<list.size();i++)
+        	{
+        		String subtype = card.getType().get(card.getType().size() - 1);
+        		CardList cl = list.getType(subtype);
+        		
+        		 if (cl.size() > 0) {
+                     return false;
+                 }
+        	}
+        }
+        if (card.isType("World")) {
+        	CardList list = AllZoneUtil.getPlayerCardsInPlay(AllZone.ComputerPlayer);
+        	list = list.getType("World");
+        	if(list.size() > 0) return false;
+        }
+        
+        if (card.isType("Creature") 
+        		&& card.getNetDefense() <= 0 
+        		&& !card.hasStartOfKeyword("etbCounter") 
+        		&& !card.getText().contains("Modular"))
+        	return false;
+        
+        if (willChampion) {
+        	Object o = championGetCreature.execute();
+            if (o == null) return false;
+            
+            CardList cl = (CardList) championGetCreature.execute();
+            if ( (o == null) || !(cl.size() > 0) || !AllZone.getZone(getSourceCard()).is(Constant.Zone.Hand))
+            	return false;
+        }
+        
+        if (!checkETBEffects(card, this, null))
+        	return false;
+        
+        return super.canPlayAI();
+    }//canPlayAI()
+    
+    public static boolean checkETBEffects(Card card, SpellAbility sa, String api){
+        // Trigger play improvements
+        ArrayList<Trigger> triggers = card.getTriggers();
+        for(Trigger tr : triggers){
+        	// These triggers all care for ETB effects
+        	
+        	HashMap<String, String> params = tr.getMapParams();
+        	if (!params.get("Mode").equals("ChangesZone"))
+        		continue;
+        	
+        	if (!params.get("Destination").equals("Battlefield"))
+        		continue;
+        	
+        	if (params.containsKey("ValidCard") && !params.get("ValidCard").contains("Self"))
+        		continue;
+        	
+        	if(!tr.requirementsCheck())
+        		continue;
+        	
+        	if (tr.getOverridingAbility() != null)	// Don't look at Overriding Abilities yet
+        		continue;
+        	
+        	// Maybe better considerations 
+        	AbilityFactory af = new AbilityFactory();
+        	SpellAbility exSA = af.getAbility(card.getSVar(params.get("Execute")), card);
+        	
+        	if (api != null && !af.getAPI().equals(api))
+        		continue;
+        	
+        	exSA.setActivatingPlayer(sa.getActivatingPlayer());
+
+        	// Run non-mandatory trigger.
+        	// These checks only work if the Executing SpellAbility is an Ability_Sub.
+        	if (exSA instanceof Ability_Sub && !exSA.doTrigger(false)){
+        		// AI would not run this trigger if given the chance
+        		
+        		// if trigger is mandatory, return false
+        		if (params.get("OptionalDecider") == null){
+        			return false;
+        		}
+        		// else
+        		// otherwise, return false 50% of the time?
+        	}
+        }    	
+    	
+    	return true;
+    }
+    
+    
+    @Override
+    public void resolve() {
+        Card c = getSourceCard();
+        AllZone.GameAction.moveToPlay(c);
+    }
+}
