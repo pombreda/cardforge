770bb8854c420d031c748fc2d0d6e323df5fe740
keybone@gmail.com <keybone@gmail.com@d79705b8-2151-0410-8572-c3d52707f174>
2011-06-27 21:01:31 +0000

    *A better fix for the CardID issue.

--- src/src/forge/card/mana/ManaPool.java
+++ src/src/forge/card/mana/ManaPool.java
@@ -38,7 +38,6 @@ public class ManaPool extends Card {
      */
     public ManaPool(Player player) {
         super();
-        setUniqueNumber(0);
         updateObservers();
         owner = player;
         this.setController(player);
