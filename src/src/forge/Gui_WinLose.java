b4708a17b47ee01e80ef778f40bc496af9b5b982
robcashwalker@gmail.com <robcashwalker@gmail.com@d79705b8-2151-0410-8572-c3d52707f174>
2011-06-27 23:48:28 +0000

    Removed BoosterPack in favor of BoosterGenerator.

--- src/src/forge/Gui_WinLose.java
+++ src/src/forge/Gui_WinLose.java
@@ -65,12 +65,18 @@ public class Gui_WinLose extends JFrame implements NewConstants {
 
         //setup limited deck
         Deck deck = new Deck(Constant.GameType.Sealed);
-        CardList pack = new CardList(BoosterPack.getBoosterPack(1).toArray());
-
-        for (int i = 0; i < pack.size(); i++)
-            if ((i % 2) == 0) deck.addSideboard(pack.get(i).getName());
-            else deck.addMain(pack.get(i).getName());
-
+        CardList pack = new CardList();
+        BoosterGenerator bg = new BoosterGenerator();
+        for (int i=0; i < 6; i++) 
+        	pack.addAll(bg.getBoosterPack()); //CardList(BoosterPack.getBoosterPack(1).toArray());
+        
+        
+        for (int i = 0; i < pack.size(); i++) {
+            if ((i % 2) == 0)
+            	deck.addSideboard(pack.get(i).getName());
+            else
+            	deck.addMain(pack.get(i).getName());
+        }
         Constant.Runtime.HumanDeck[0] = deck;
         //end - setup limited deck
 
