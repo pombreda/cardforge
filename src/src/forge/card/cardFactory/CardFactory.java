d1cc6da62b549d12674b4633b3b83bf27e66d47a
jendave.hudson@gmail.com <jendave.hudson@gmail.com@d79705b8-2151-0410-8572-c3d52707f174>
2011-06-29 09:32:43 +0000

    Moved test-like methods/classes into the unit test directories. very quick and dirty

--- src/src/forge/card/cardFactory/CardFactory.java
+++ src/src/forge/card/cardFactory/CardFactory.java
@@ -3066,15 +3066,4 @@ public class CardFactory implements NewConstants {
 
         return c;
     }// copyStats()
-
-    /**
-     * <p>main.</p>
-     *
-     * @param args an array of {@link java.lang.String} objects.
-     */
-    public static void main(String[] args) {
-        CardFactory f = new CardFactory(ForgeProps.getFile(CARDSFOLDER));
-        Card c = f.getCard("Arc-Slogger", null);
-        System.out.println(c.getOwner());
-    }
 }
