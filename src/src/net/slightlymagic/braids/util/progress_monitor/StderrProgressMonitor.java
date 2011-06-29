c34b6dd2ec2c0bfaffc1182e9423ca1aac652ffa
BraidsCC@gmail.com <BraidsCC@gmail.com@d79705b8-2151-0410-8572-c3d52707f174>
2011-06-29 22:35:14 +0000

    minor changes to net.slightlymagic.braids.util.progress_monitor classes: made currentPhaseExponent a float (bugfix), BaseProgressMonitor now calls its own setTotalUnitsThisPhase more consistently, fix to BaseProgressMonitor's log message that helps determine phase weights, and plenty of javadoc improvements.

--- src/src/net/slightlymagic/braids/util/progress_monitor/StderrProgressMonitor.java
+++ src/src/net/slightlymagic/braids/util/progress_monitor/StderrProgressMonitor.java
@@ -2,16 +2,25 @@ package net.slightlymagic.braids.util.progress_monitor;
 
 public class StderrProgressMonitor extends BaseProgressMonitor {
 
+	/**
+	 * @see BaseProgressMonitor(int,long)
+	 */
     public StderrProgressMonitor(int numPhases, long totalUnitsFirstPhase) {
     	this(numPhases, totalUnitsFirstPhase, 2.0f, null);
     }
     
+	/**
+	 * @see BaseProgressMonitor(int,long,float)
+	 */
     public StderrProgressMonitor(int numPhases, long totalUnitsFirstPhase, 
             float minUpdateIntervalSec) 
 	{
 		this(numPhases, totalUnitsFirstPhase, minUpdateIntervalSec, null);
 	}
     
+	/**
+	 * @see BaseProgressMonitor(int,long,float,float[])
+	 */
     public StderrProgressMonitor(int numPhases, long totalUnitsFirstPhase, 
                  float minUpdateIntervalSec, float[] phaseWeights) 
     {
@@ -19,6 +28,9 @@ public class StderrProgressMonitor extends BaseProgressMonitor {
     }
     
     @Override
+	/**
+	 * @see BaseProgressMonitor#incrementUnitsCompletedThisPhase(long)
+	 */
     public void incrementUnitsCompletedThisPhase(long numUnits) {
         super.incrementUnitsCompletedThisPhase(numUnits);
 
@@ -43,6 +55,13 @@ public class StderrProgressMonitor extends BaseProgressMonitor {
         }
      }
 
+    /**
+     * Displays a message to stderr, overwriting the current estimate; calls
+     * from outside this class should provide a newline character at the 
+     * end of the messsage.
+     * 
+     * @param message  the message to display
+     */
     public void printUpdate(String message) {
     	
     	while (message.length() < 79) {
