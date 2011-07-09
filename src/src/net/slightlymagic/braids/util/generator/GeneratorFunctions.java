dacd2312b5d5e6524882141d90b22743a28d21bf
BraidsCC@gmail.com <BraidsCC@gmail.com@d79705b8-2151-0410-8572-c3d52707f174>
2011-07-09 21:10:00 +0000

    Update some general utilities in preparation for my next commit. Add two functions to GeneratorFunctions for performing lazy deferred operations on products of a generator.

--- src/src/net/slightlymagic/braids/util/generator/GeneratorFunctions.java
+++ src/src/net/slightlymagic/braids/util/generator/GeneratorFunctions.java
@@ -1,8 +1,11 @@
 /** Licensed under both the GPL and the Apache 2.0 License. */
 package net.slightlymagic.braids.util.generator;
 
+import net.slightlymagic.braids.util.lambda.Lambda1;
+
 import com.google.code.jyield.Generator;
 import com.google.code.jyield.YieldUtils;
+import com.google.code.jyield.Yieldable;
 
 /**
  * For documentation on Java-Yield and its generators, see
@@ -37,6 +40,87 @@ public final class GeneratorFunctions {
 		{
 			result++;
 		}
+		
+		return result;
+	}
+
+	/**
+	 * Highly efficient means of filtering a long or infinite sequence.
+	 * 
+	 * @param <T> any type
+	 * 
+	 * @param predicate a Lambda (function) whose apply method takes an object 
+	 * of type <T> and returns a Boolean. If it returns false or null, the 
+	 * item from the inputGenerator is not yielded by this Generator;
+	 * if predicate.apply returns true, then this Generator <i>does</i>
+	 * yield the value. 
+	 * 
+	 * @param inputGenerator the sequence upon which we operate
+	 * 
+	 * @return a generator which produces a subset <= the inputGenerator
+	 */
+	public static <T> Generator<T> filterGenerator(
+			final Lambda1<Boolean,T> predicate, final Generator<T> inputGenerator) 
+	{
+		Generator<T> result = new Generator<T>() {
+
+			@Override
+			public void generate(final Yieldable<T> outputYield) {
+
+				Yieldable<T> inputYield = new Yieldable<T>() {
+					Boolean pResult;
+					
+					@Override
+					public void yield(T input) {
+						pResult = predicate.apply(input); 
+						if (pResult != null && pResult) {
+							outputYield.yield(input);
+						}
+					}
+				};
+				
+				inputGenerator.generate(inputYield);
+			}
+			
+		};
+		
+		return result;
+	}
+
+	/**
+	 * Highly efficient means of applying a transform to a long or infinite 
+	 * sequence.
+	 * 
+	 * @param <T> any type
+	 * 
+	 * @param transform a Lambda (function) whose apply method takes an object 
+	 * of type <T> and returns an object of the same type. This transforms
+	 * the values from the inputGenerator into this Generator.
+	 * 
+	 * @param inputGenerator the sequence upon which we operate
+	 * 
+	 * @return a generator that yields transform.apply's return value for
+	 * each item in the inputGenerator
+	 */
+	public static <T> Generator<T> transformGenerator(
+			final Lambda1<T,T> transform, final Generator<T> inputGenerator) 
+	{
+		Generator<T> result = new Generator<T>() {
+
+			@Override
+			public void generate(final Yieldable<T> outputYield) {
+
+				Yieldable<T> inputYield = new Yieldable<T>() {
+					@Override
+					public void yield(T input) {
+						outputYield.yield(transform.apply(input));
+					}
+				};
+				
+				inputGenerator.generate(inputYield);
+			}
+			
+		};
 		
 		return result;
 	}
