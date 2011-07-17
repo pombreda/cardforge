3d2ee1f7be4c3f86c8a3e564cb3731f38421f203
dennis.r.friedrichsen <dennis.r.friedrichsen@d79705b8-2151-0410-8572-c3d52707f174>
2011-07-17 14:02:27 +0000

    cleanup in GuiDisplay4

--- src/src/forge/GuiDisplay4.java
+++ src/src/forge/GuiDisplay4.java
@@ -6,7 +6,6 @@ import arcane.ui.PlayArea;
 import arcane.ui.ViewPanel;
 import arcane.ui.util.Animation;
 import forge.card.cardFactory.CardFactoryUtil;
-import forge.card.spellability.SpellAbility;
 import forge.error.ErrorViewer;
 import forge.gui.ForgeAction;
 import forge.gui.GuiUtils;
